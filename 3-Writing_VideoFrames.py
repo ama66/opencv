ba8f2593a20dd6e9df7a31284f9944ebed5f68ba
Ammar Abdilghanie <ama66@cornell.edu>
2020-08-04 11:04:04 -0500

    Update 3-Writing_VideoFrames.py

--- 3-Writing_VideoFrames.py
+++ 3-Writing_VideoFrames.py
@@ -1,55 +1,35 @@
-#
+import cv2 as cv
 
-import cv2 as cv 
-import numpy as np 
+## opening videocapture
+cap = cv.VideoCapture(0)
 
+## some videowriter props
+sz = (int(cap.get(cv.CAP_PROP_FRAME_WIDTH)),
+        int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)))
 
-cap=cv.VideoCapture(0)
-# you could also read existing video file xx.avi 
-# and process it e.g. flip it, cv.VideoCapture('xx.avi') where xx.avi is in the same directory
+fps = 20
+#fourcc = cv.VideoWriter_fourcc('m', 'p', '4', 'v')
+#fourcc = cv.VideoWriter_fourcc('m', 'p', 'e', 'g')
+fourcc = cv.VideoWriter_fourcc(*'mp4v')
 
-# print(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-# print(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-# # width 1280.0
-# # Height 720.0
+## open and set props
+vout = cv.VideoWriter()
+vout.open('output.mp4',fourcc,fps,sz,True)
 
-# cap.set(cv.CAP_PROP_FRAME_WIDTH,450)
-# cap.set(cv.CAP_PROP_FRAME_HEIGHT,300)
-
-# print("New Dimensions \n")
-# print(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-# print(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-
-# width 640.0
-# Height 480.0
-
-## Define Codec for generated video 
-
-fourcc=cv.VideoWriter_fourcc(*'XVID')
-## Filename is output.avi 
-
-out=cv.VideoWriter('output.avi',fourcc,20 , (640,480),True ) 
-## 20 frame per sec, size, corlor=True
-
+cnt = 0
 while cap.isOpened():
-
-    ret,frame=cap.read()
-
+    ret, frame = cap.read()
     if not ret:
         print("Cannot receive frames")
         break
-
     frame=cv.flip(frame,0)
-    out.write(frame)
+    vout.write(frame)
     cv.imshow("colored video",frame)
     if cv.waitKey(1)==ord("q"):
         break
+
     
 
+vout.release()
 cap.release()
-out.release()
-cv.destroyAllWindows()
-
-
-
 
